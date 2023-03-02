c476faec2da112ac64de7f96889237b9e0321a33
KDSingh <rajputkdsingh999@gmail.com>
2023-03-02 18:44:58 +0530

    change is postControler

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,54 +1,59 @@
-const Post=require('../modals/post');
-const Categories=require('../modals/categories')
+const Post = require("../modals/post");
+const Categories = require("../modals/categories");
 
-module.exports={
-    //admin---set
-    postCreate:async (req,res)=>{
-        try {
-            
-         const post=req.body
-         const newPost=await Post.create(post);
-         await Categories.updateMany({ '_id': newPost.categories }, { $push: { posts: newPost._id } });
-         return res.status(201).json(newPost) 
-        } catch (error) {
-            return res.status(500).json({error:"can not be stored"});
-        }
-
-    },
-    allPosts:async(req,res)=>{
-        try {
-            const posts=await Post.find().populate('categories');
-            return res.status(201).json(posts);
-        } catch (error) {
-            return res.send(500).json(error);
-            
-        }
-
-    },
-    getPost:async(req,res)=>{
-        try {
-            const id=req.params.id;
-            const post=await Post.findById(id).populate('categories');
-            return res.status(201).json(post);
-            
-        } catch (error) {
-            return res.status(500).json()
-            
-        }
-
-    },
-    updatePost:async(req,res)=>{
-        try {
-            const id=req.params.id;
-            if(!id){
-                return res.status(404).json({message:"user not found"});
-            }
-            const updatePost=await Post.findByIdAndUpdate(id,req.body);
-            return res.status(201).json({message:"post updated",updatePost})
-        } catch (error) {
-            return res.status(500).json(error);
-            
-        }
+module.exports = {
+  //admin---set
+  postCreate: async (req, res) => {
+    try {
+      const post = req.body;
+      const newPost = await Post.create(post);
+      await Categories.updateMany(
+        { _id: newPost.categories },
+        { $push: { posts: newPost._id } }
+      );
+      return res.status(201).json(newPost);
+    } catch (error) {
+      return res.status(500).json({ error: "can not be stored" });
     }
-  
-}
+  },
+  allPosts: async (req, res) => {
+    try {
+      const { page = 1, limit = 9 } = req.query;
+
+      const posts = await Post.find()
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      // .exex();
+      const count = await Post.countDocuments();
+
+      return res.status(201).json({
+        posts,
+        totalPages: Math.ceil(count / limit),
+        currentPage: page,
+      });
+    } catch (error) {
+      return res.send(500).json(error);
+    }
+  },
+  getPost: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const post = await Post.findById(id).populate("categories");
+      return res.status(201).json({ post: post });
+    } catch (error) {
+      return res.status(500).json();
+    }
+  },
+  updatePost: async (req, res) => {
+    try {
+      const id = req.params.id;
+      if (!id) {
+        return res.status(404).json({ message: "user not found" });
+      }
+      const updatePost = await Post.findByIdAndUpdate(id, req.body);
+      return res.status(201).json({ message: "post updated", updatePost });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  },
+};
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,24 @@
-const express=require('express');
-const mongoose=require('mongoose');
-require('dotenv').config();
-const postrouter=require('./route/post')
-const categoryrouter=require('./route/category');
-const userrouter=require('./route/user')
-const cors=require('cors');
-const cookieParser=require('cookie-parser');
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const postrouter = require("./route/post");
+const categoryrouter = require("./route/category");
+const userrouter = require("./route/user");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
 app.use(cors());
 app.use(cookieParser());
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.DB_URL,(err)=>{
-    if(!err) console.log("database connected")
-})
-
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DB_URL, (err) => {
+  if (!err) console.log("database connected");
+});
 
 app.use(express.json());
-app.use('/api/vi/post',postrouter);
-app.use('/api/vi/category',categoryrouter);
-app.use('/api/vi/user',userrouter)
+app.use("/api/vi/post", postrouter);
+app.use("/api/vi/category", categoryrouter);
+app.use("/api/vi/user", userrouter);
 
-app.listen(process.env.PORT,(err)=>{
-    if(!err) console.log(`server is started ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, (err) => {
+  if (!err) console.log(`server is started ${process.env.PORT}`);
+});
