366ba6107666089d0024f350833e0da4ebd586a0
KDSingh <rajputkdsingh999@gmail.com>
2023-03-18 14:29:22 +0530

    changes api index

--- h1blogs/pages/api/index.js
+++ h1blogs/pages/api/index.js
@@ -6,18 +6,47 @@ import axios from "axios";
 //     "Content-Type": "application/json",
 //   },
 // });
-if (typeof window !== "undefined") {
-  // Perform localStorage action
-  const token = localStorage.getItem("token");
-  axios.defaults.baseURL = "http://localhost:5000/api/vi";
-  axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
+// if (typeof window !== "undefined") {
+//   // Perform localStorage action
+//   const token = localStorage.getItem("token");
+//   axios.defaults.baseURL = "http://localhost:5000/api/vi";
+//   axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
 
-  // console.log(axios.headers);
-}
+//   // console.log(axios.headers);
+// }
 
 // //const token = localStorage.getItem("token");
 // //console.log(token);
 
 // export default instance;
 
-export default axios;
+// export default axios;
+
+// import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/api/vi",
+});
+instance.defaults.headers.common["Content-Type"] = "multipart/form-data";
+
+//validate response
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401 || error.response.status === 403) {
+      return (window.location.href = "/admin");
+    }
+    return Promise.reject(error);
+  }
+);
+
+// Set the AUTH token for any request
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
+});
+
+export default instance;
