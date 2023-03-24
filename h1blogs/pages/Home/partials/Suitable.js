67892b95301ab061c7679a448f9a56150ac8b730
KDSingh <rajputkdsingh999@gmail.com>
2023-03-24 09:55:03 +0530

    changes in suitable.js

--- h1blogs/pages/Home/partials/Suitable.js
+++ h1blogs/pages/Home/partials/Suitable.js
@@ -7,6 +7,7 @@ import {
   CardBody,
   Text,
   Stack,
+  Highlight,
 } from "@chakra-ui/react";
 import React from "react";
 
@@ -14,7 +15,12 @@ export const Suitable = () => {
   return (
     <Container maxW={"full"} px={[5, 10]}>
       <Heading textAlign={"center"} py={10}>
-        This Course is suitable for
+        <Highlight
+          query="suitable"
+          styles={{ px: "1", py: "1", color: "#dc3545" }}
+        >
+          This Course is suitable for
+        </Highlight>
       </Heading>
       <SimpleGrid
         columns={[1, 3]}
@@ -27,7 +33,14 @@ export const Suitable = () => {
           <Card>
             <CardBody>
               <Stack mt="6" spacing="3">
-                <Heading size="md">⦁ Students/Fresher’s</Heading>
+                <Heading size="md">
+                  <Highlight
+                    query="⦁ Students/Fresher’s"
+                    styles={{ px: "1", py: "1", color: "#dc3545" }}
+                  >
+                    ⦁ Students/Fresher’s
+                  </Highlight>
+                </Heading>
                 <Text>
                   College students interested in pursuing a job in Digital
                   Marketing
@@ -40,7 +53,14 @@ export const Suitable = () => {
           <Card>
             <CardBody>
               <Stack mt="6" spacing="3">
-                <Heading size="md">⦁ Professionals</Heading>
+                <Heading size="md">
+                  <Highlight
+                    query="⦁ Professionals"
+                    styles={{ px: "1", py: "1", color: "#dc3545" }}
+                  >
+                    ⦁ Professionals
+                  </Highlight>
+                </Heading>
                 <Text>
                   Professionals seeking to advance their jobs in the more
                   hopeful field of Digital Marketing.
@@ -53,7 +73,15 @@ export const Suitable = () => {
           <Card>
             <CardBody>
               <Stack mt="6" spacing="3">
-                <Heading size="md">⦁ Coaches</Heading>
+                <Heading size="md">
+                  <Highlight
+                    query="⦁ Coaches"
+                    styles={{ px: "1", py: "1", color: "#dc3545" }}
+                  >
+                    ⦁ Coaches
+                  </Highlight>
+                </Heading>
+
                 <Text>
                   Coaches and trainers who want to expand their training company
                   online and attract more students.
@@ -66,7 +94,14 @@ export const Suitable = () => {
           <Card>
             <CardBody>
               <Stack mt="6" spacing="3">
-                <Heading size="md">⦁ Homepreneurs</Heading>
+                <Heading size="md">
+                  <Highlight
+                    query="⦁ Homepreneurs"
+                    styles={{ px: "1", py: "1", color: "#dc3545" }}
+                  >
+                    ⦁ Homepreneurs
+                  </Highlight>
+                </Heading>
                 <Text>
                   Homepreneurs who want to improve their digital marketing
                   skills and create a source of passive revenue.
@@ -79,7 +114,15 @@ export const Suitable = () => {
           <Card>
             <CardBody>
               <Stack mt="6" spacing="3">
-                <Heading size="md">⦁ Agency Owners</Heading>
+                <Heading size="md">
+                  <Highlight
+                    query="⦁ Agency Owners"
+                    styles={{ px: "1", py: "1", color: "#dc3545" }}
+                  >
+                    ⦁ Agency Owners
+                  </Highlight>
+                </Heading>
+
                 <Text>
                   Entrepreneurs and agency owners who want to develop their
                   companies and produce leads on digital platforms.
@@ -89,10 +132,17 @@ export const Suitable = () => {
           </Card>
         </Box>
         <Box>
-          <Card maxW="sm">
+          <Card maxW="">
             <CardBody>
               <Stack mt="6" spacing="3">
-                <Heading size="md">⦁ Freelancers</Heading>
+                <Heading size="md">
+                  <Highlight
+                    query="⦁ Freelancers"
+                    styles={{ px: "1", py: "1", color: "#dc3545" }}
+                  >
+                    ⦁ Freelancers
+                  </Highlight>
+                </Heading>
                 <Text>
                   Freelance marketers seeking more national and foreign
                   customers and a stable income.
