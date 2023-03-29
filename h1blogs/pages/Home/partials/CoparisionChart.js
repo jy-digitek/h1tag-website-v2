d1ddde5db43204acbe58489296d5102d6a05a3ce
KDSingh <krishnadevsinghjytech@gmail.com>
2023-03-29 13:53:55 +0530

    changes in components

--- h1blogs/pages/Home/partials/CoparisionChart.js
+++ h1blogs/pages/Home/partials/CoparisionChart.js
@@ -11,14 +11,6 @@ import {
   Text,
   Center,
 } from "@chakra-ui/react";
-import { MdCurrencyRupee, MdOutlineCastForEducation } from "react-icons/md";
-import { BsFillHandbagFill } from "react-icons/bs";
-import {
-  FaUserGraduate,
-  FaChalkboardTeacher,
-  FaHistory,
-  FaCogs,
-} from "react-icons/fa";
 
 import { comparision } from "../../../components/DATA";
 export const ComparisionChar = () => {
@@ -60,107 +52,6 @@ export const ComparisionChar = () => {
             </Box>
           );
         })}
-
-        {/* <Box>
-          <Card maxW="sm">
-            <CardBody>
-              <Center>
-                {" "}
-                <FaChalkboardTeacher size={"100px"} />
-              </Center>
-
-              <Stack mt="6" spacing="3">
-                <Heading size="md" color={"#101C32"}>
-                  Training Facilities
-                </Heading>
-                <Text>
-                  Easy for students to understand and study. Keen students can
-                  stay updated with their personalized Laptops relating to their
-                  subjects' latest advancements and curriculum.
-                </Text>
-              </Stack>
-            </CardBody>
-          </Card>
-        </Box>
-        <Box>
-          <Card maxW="sm">
-            <CardBody>
-              <Center>
-                <BsFillHandbagFill size={"100px"} />
-              </Center>
-
-              <Stack mt="6" spacing="3">
-                <Heading size="md" color={"#101C32"}>
-                  Job Placement
-                </Heading>
-                <Text>
-                  Students participating in placement units learn about business
-                  standards and improve their job prospects. We give assured
-                  placement guarantee to our students.
-                </Text>
-              </Stack>
-            </CardBody>
-          </Card>
-        </Box>
-        <Box>
-          <Card maxW="sm">
-            <CardBody>
-              <Center>
-                <FaUserGraduate size={"100px"} />
-              </Center>
-              <Stack mt="6" spacing="3">
-                <Heading size="md" color={"#101C32"}>
-                  Paid Internship
-                </Heading>
-                <Text>
-                  We ensure that our students get paid internships from the
-                  first month of joining. Unlike many of your friends, you can
-                  begin building your collection sooner. You can add this
-                  additional expertise to your resume.
-                </Text>
-              </Stack>
-            </CardBody>
-          </Card>
-        </Box>
-        <Box>
-          <Card maxW="sm">
-            <CardBody>
-              <Center>
-                <FaCogs size={"100px"} />
-              </Center>
-
-              <Stack mt="6" spacing="3">
-                <Heading size="md" color={"#101C32"}>
-                  Advanced Experience
-                </Heading>
-                <Text>
-                  As H1 Tags is an agency-based institute, we ensure that our
-                  students work on practical and live projects, which builds
-                  confidence.
-                </Text>
-              </Stack>
-            </CardBody>
-          </Card>
-        </Box>
-        <Box>
-          <Card maxW="sm">
-            <CardBody>
-              <Center>
-                <FaHistory size={"100px"} />
-              </Center>
-              <Stack mt="6" spacing="3">
-                <Heading size="md" color={"#101C32"}>
-                  Proven Track Record
-                </Heading>
-                <Text>
-                  Our students tend to grow and enhance their careers; in many
-                  ways, our students are happy and satisfied as with our
-                  institute they got placement in well-known companies.
-                </Text>
-              </Stack>
-            </CardBody>
-          </Card>
-        </Box> */}
       </SimpleGrid>
     </Container>
   );
