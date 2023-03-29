5ba8a8cd97ba3094a2ec42887af63f9e50965bf9
JY-TechIN <gautamjytech@gmail.com>
2023-03-29 13:06:28 +0530

    Create seperate data set for all the repetitive items

---
+++ h1blogs/components/DATA.js
@@ -0,0 +1,32 @@
+export const Benefits = [
+  {
+    label: "Earn While You Learning",
+  },
+  {
+    label: "100% Practical Course",
+  },
+  {
+    label: "Classroom Training",
+  },
+  {
+    label: "20+ Globally Recognized Certifications",
+  },
+  {
+    label: "EMIs Available",
+  },
+  {
+    label: "100% Placement Assistance",
+  },
+  {
+    label: "Grow Your Startup Business",
+  },
+  {
+    label: "Paid Internship from 1st Month",
+  },
+  {
+    label: "Live Projects",
+  },
+  {
+    label: "Weekday and Weekend Batches",
+  },
+];
--- h1blogs/pages/Home/partials/Benifits.js
+++ h1blogs/pages/Home/partials/Benifits.js
@@ -11,6 +11,8 @@ import {
 import styles from "../../../styles/Home.module.css";
 import { SectionTitle } from "../../../components/SectionTitle";
 
+import { Benefits } from "../../../components/DATA";
+
 export const Benifits = () => {
   return (
     <Container
@@ -22,66 +24,13 @@ export const Benifits = () => {
       <SectionTitle>Benefits to Students</SectionTitle>
 
       <Wrap spacing={["10px", "20px"]} py={5} px={[0, 10]} fontSize={"20px"}>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>Earn While You Learning</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>100% Practical Course</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>50+ Modules</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>Classroom Training</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>20+ Globally Recognized Certifications</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>EMIs Available</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>100% Placement Assistance </Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>Grow Your Startup Business </Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>Paid Internship from 1st Month</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>Laptop Facility Available</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>Live Projects</Center>
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
-            <Center>Weekday and Weekend Batches</Center>
-          </Box>
-        </WrapItem>
+        {Benefits.map((item, key) => (
+          <WrapItem key={key}>
+            <Box boxShadow="2px 0 35px 0 rgb(68 88 144 / 12%)" p={[2, 5]}>
+              <Center>{item.label}</Center>
+            </Box>
+          </WrapItem>
+        ))}
       </Wrap>
       <Center>
         <Button px={20} py={5} rounded={20} bg={"yellow"}>
