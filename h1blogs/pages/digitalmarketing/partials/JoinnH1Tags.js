0a6e4a4c8801453b69a4fa629f3615861d7ab4ce
KDSingh <krishnadevsinghjytech@gmail.com>
2023-04-06 18:38:30 +0530

    changes in JoiningTags

--- h1blogs/pages/digitalmarketing/partials/JoinnH1Tags.js
+++ h1blogs/pages/digitalmarketing/partials/JoinnH1Tags.js
@@ -14,16 +14,16 @@ import { FcManager, FcRules, FcAssistant, FcSettings } from "react-icons/fc";
 
 export const JoinH1Tags = () => {
   return (
-    <Container maxW={"full"} py={5}>
-      <SectionTitle children={"Benefits of Joining H1 Tags"} my={10} />
-      <Box as="p" my={[3, 5]} maxW={"5xl"} mx={"auto"}>
+    <Container maxW={"full"} py={10} bg="gray.100">
+      <SectionTitle children={"Benefits of Joining H1 Tags"} />
+      <Box as="p" my={[3, 5]} maxW={"5xl"} mx={"auto"} textAlign={"center"}>
         Get a globally recognized Digital Marketing Certification. Advance your
         marketing profession by enhancing your digital abilities with the best
         Digital Marketing Training Institute in Delhi.
       </Box>
       <SimpleGrid columns={[1, 4]} spacing={10} px={10}>
         <Box as="card" boxShadow={"sm"}>
-          <Card>
+          <Card borderLeft={"3px solid blue"}>
             <Flex>
               <CardHeader>
                 <FcManager fontSize={50} />
@@ -33,7 +33,7 @@ export const JoinH1Tags = () => {
           </Card>
         </Box>
         <Box boxShadow={"sm"}>
-          <Card>
+          <Card borderLeft={"3px solid blue"}>
             <Flex>
               <CardHeader>
                 {" "}
@@ -44,7 +44,7 @@ export const JoinH1Tags = () => {
           </Card>
         </Box>
         <Box boxShadow={"sm"}>
-          <Card>
+          <Card borderLeft={"3px solid blue"}>
             <Flex>
               <CardHeader>
                 {" "}
@@ -56,7 +56,7 @@ export const JoinH1Tags = () => {
           </Card>
         </Box>
         <Box boxShadow={"sm"}>
-          <Card>
+          <Card borderLeft={"3px solid blue"}>
             {" "}
             <Flex>
               <CardHeader>
