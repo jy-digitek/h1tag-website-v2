cc8447890b74bc5ec4c6b13945fe8a56e08bb36f
JY-TechIN <gautamjytech@gmail.com>
2023-04-06 13:29:36 +0530

    Update

--- h1blogs/pages/digitalmarketing/index.js
+++ h1blogs/pages/digitalmarketing/index.js
@@ -1,29 +1,6 @@
+import React from "react";
 import Layout from "../../components/layout";
-import { CourseTab } from "./partials/CourseTab";
-import { Faq } from "./partials/Faq";
-import { JoinH1Tags } from "./partials/JoinnH1Tags";
-import { CarrerOption } from "./partials/CarrerOption";
-import { EligibalForCourse } from "./partials/EligibalForCourse";
-import { ScholarshipProgram } from "./partials/ScholarshipProgram";
-import { WithSpeechBubbles } from "./partials/Testimonial";
-import { DigitalmarketingBox } from "./partials/DigitalmarketingBox";
-import { H1TagsAdmission } from "./partials/H1TagsAdmission";
 
-const index = () => {
-  return (
-    <Layout>
-      <DigitalmarketingBox />
-      <CourseTab />
-      <JoinH1Tags />
-      <CarrerOption />
-      <EligibalForCourse />
-      <ScholarshipProgram />
-      <WithSpeechBubbles />
-      <H1TagsAdmission />
-
-      <Faq />
-    </Layout>
-  );
-};
-
-export default index;
+export default function index() {
+  return;
+}
