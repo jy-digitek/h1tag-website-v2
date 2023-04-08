c8fbe5d6219aa73e830c3db2702a25a9851ccbf3
KDSingh <krishnadevsinghjytech@gmail.com>
2023-04-08 11:35:18 +0530

    change in Eligibility ScholarShipProgram

--- h1blogs/pages/graphicdesign/partials/ScholarShipProgram.js
+++ h1blogs/pages/graphicdesign/partials/ScholarShipProgram.js
@@ -1,6 +1,46 @@
 import React from "react";
-import { Container } from "@chakra-ui/react";
+import { Container, Box, Text, Heading, Center, Image } from "@chakra-ui/react";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { GlobalButton } from "../../../components/GlobalButton";
 
 export const ScholarShipProgram = () => {
-  return <Container>ScholarShipProgram</Container>;
+  return (
+    <Box
+    // background={" #3950a1 url(/bg-p.jpg)"}
+    // backgroundPosition={"cover"}
+    // backgroundRepeat={"no-repeat"}
+    // backgroundColor={"#3950a1"}
+    //opacity={".7"}
+    >
+      <Container maxw="full" position={"relative"}>
+        <Box>
+          <Image src="/bg-p.jpg" width={"full"} />
+        </Box>
+
+        <Box position={"absolute"} right={10} zIndex={1} top={10}>
+          <SectionTitle children="Our Scholarship Program" />
+          <Text textAlign={"center"} fontWeight={"normal"}>
+            Our scholarship program is for those performing well academically
+            but not financially stable. This program is based on your percentage
+            and marks, eliminating financial issues. We are the only Graphic
+            Design Training Institute in Delhi with a scholarship program
+          </Text>
+          <Heading size={"sm"} textAlign={"center"} py={3}>
+            Don’t let anything come in between your dreams
+          </Heading>
+          <Center>
+            <GlobalButton
+              rounded={"full"}
+              color={"white"}
+              label={" Apply Now"}
+              background="#3950a1"
+              _hover={{ background: "#101C32" }}
+              mt={2}
+              px={10}
+            />
+          </Center>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
