08a1e2f3b3c4ffd8d40363bdb1da8bb9b7446f58
KDSingh <krishnadevsinghjytech@gmail.com>
2023-04-12 16:04:24 +0530

    complete cta

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const jwtSecret =
   "sjvdcjcsdmcbvjhsvcjhydsmnbsxc,vsxgdcvghsdvdc,nbvcmv ,xvccjvccjhdsv";
+const nodemailer = require("nodemailer");
 
 module.exports = {
   registerUser: async (req, res) => {
@@ -75,6 +76,49 @@ module.exports = {
       return res.status(200).json({
         success: true,
         message: "logout successfully",
+      });
+    } catch (error) {}
+  },
+  gmail: async (req, res) => {
+    const body = req.body.values;
+    console.log(body);
+    try {
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        requireTLS: true,
+        auth: {
+          user: "rajputkdsingh999@gmail.com", // generated ethereal user
+          pass: "iqxbwwjlavrimfqo", // generated ethereal password
+        },
+      });
+      var mailOptions = {
+        from: "rajputkdsingh999@gmail.com",
+        to: "krishnadevsinghjytech@gmail.com",
+        subject: "Hello ✔", // Subject line
+        text: "Hello world? How are you", // plain text body
+        html: `<div style="background-color: lightgray; width:400px;height:250px;">
+        <img src="https://h1tags.com/Logo-01.png" style="width:100px;height:100px;margin-left:30%;"/>
+        <div style="margin-left:10px;">
+        <h3>name:${body.name}</h3>
+        <h3>conatct:${body.phone}</h3> 
+         <h3>email:${body.email}</h3>
+         <h3>place:${body.place}</h3>
+         </div>
+       
+        </div>
+       `,
+
+        // html body
+      };
+      await transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(info);
+          res.send(info);
+        }
       });
     } catch (error) {}
   },
